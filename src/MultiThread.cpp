0e3c90edca9b0dbcf567796817a47bb5fd1cdafc
Ronnie E <rel-alam@hotmail.com>
2015-03-06 16:41:43 +1100

    Multi threading

---
+++ src/MultiThread.cpp
@@ -0,0 +1,34 @@
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <cstdio>
+#include "gl_core_4_4.h"
+#include "glm_header.h"
+
+int main()
+{
+
+
+	std::vector<std::thread> threads;
+	vec4 myVectors[50000];
+	int chunkLength = 50000 / 10;
+
+	for (int i = 0; i < 10; i++) {
+		threads.push_back(std::thread(
+			// defining a lambda that can access myMutex
+			[&](int low, int high){
+			for (int j = low; j < high; j++)
+			{
+				myVectors[i] = glm::normalize(myVectors[i]);
+				printf(myVectors[i]);
+			}
+		}, i * chunkLength, (i+1) * chunkLength
+		));
+	}
+	for (auto& thread : threads)
+		thread.join();
+
+	system("pause");
+
+	return 0;
+}
