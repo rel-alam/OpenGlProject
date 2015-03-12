87fdd2c879d04bd3d9a207923cbc0c96dbab9066
Ronnie E <rel-alam@hotmail.com>
2015-03-12 16:55:54 +1100

    +

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
-//#include "gl_core_4_4.h"
-//
-//#include <GLFW\glfw3.h>
-//#include <cstdio>
-//#include "Gizmos.h"
-//#define GLM_SWIZZLE
-//#include "glm/glm.hpp"
-//#include "glm/ext.hpp"
-//
-//#include "BoundingTest.h"
-//
-//int main()
-//{
-//
-//	BoundingTest app;
-//
-//	if (app.startup() == false)
-//	{
-//		return -1;
-//	}
-//
-//	while (app.update() == true)
-//	{
-//		app.draw();
-//	}
-//
-//	app.shutdown();
-//
-//	return 0;
-//}
+#include "gl_core_4_4.h"
+
+#include <GLFW\glfw3.h>
+#include <cstdio>
+#include "Gizmos.h"
+#define GLM_SWIZZLE
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+
+#include "BoundingTest.h"
+
+int main()
+{
+
+	BoundingTest app;
+
+	if (app.startup() == false)
+	{
+		return -1;
+	}
+
+	while (app.update() == true)
+	{
+		app.draw();
+	}
+
+	app.shutdown();
+
+	return 0;
+}
